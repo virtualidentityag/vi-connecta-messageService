caf2db1284b30b4f65bfda789c9584985a5255a6
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-04-04 14:29:01 +0200

    feat: caritas-422 improve sentry implementation for message service

--- src/main/java/de/caritas/cob/messageservice/scheduler/RocketChatCredentialsHelperScheduler.java
+++ src/main/java/de/caritas/cob/messageservice/scheduler/RocketChatCredentialsHelperScheduler.java
@@ -2,9 +2,11 @@ package de.caritas.cob.messageservice.scheduler;
 
 import de.caritas.cob.messageservice.api.service.LogService;
 import de.caritas.cob.messageservice.api.service.helper.RocketChatCredentialsHelper;
+import javax.annotation.PostConstruct;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,13 +16,13 @@ public class RocketChatCredentialsHelperScheduler {
 
   private final @NonNull RocketChatCredentialsHelper rcCredentialsHelper;
 
-//  @PostConstruct
+  @PostConstruct
   public void postConstructInitializer() {
     LogService.logDebug("RocketChatCredentialsHelperScheduler - initialize tokens");
     rcCredentialsHelper.updateCredentials();
   }
 
-  // @Scheduled(cron = "${rocket.credentialscheduler.cron}")
+  @Scheduled(cron = "${rocket.credentialscheduler.cron}")
   public void scheduledRotateToken() {
     LogService.logDebug("RocketChatCredentialsHelperScheduler - rotating tokens");
     rcCredentialsHelper.updateCredentials();
